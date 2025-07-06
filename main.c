c0bfbdb47218814302201c1aa4cee81b812418c6
Abdur Rafiu Bhuiyan <abdurrafiub673@gmail.com>
2025-07-07 01:15:35 +0600

    main.c

--- main.c
+++ main.c
@@ -19,6 +19,9 @@ Direction directions[] = {
     {1, 1}, {1, -1}, {-1, 1}, {-1, -1}
 };
 
+
+
+
 int is_valid_position(int x, int y, int dx, int dy, const char* word) {    //To be continued
     int len = strlen(word);
     for (int i = 0; i < len; i++) {
@@ -30,4 +33,32 @@ int is_valid_position(int x, int y, int dx, int dy, const char* word) {    //To 
             return 0;
     }
     return 1;
+}
+
+
+
+
+
+void place_word(const char* word) {
+    int placed = 0, attempts = 100;
+    int len = strlen(word);
+    while (!placed && attempts--) {
+        int dir_index = rand() % 8;
+        Direction dir = directions[dir_index];
+        int x = rand() % GRID_SIZE;
+        int y = rand() % GRID_SIZE;
+
+        if (is_valid_position(x, y, dir.dx, dir.dy, word)) {
+            for (int i = 0; i < len; i++) {
+                int nx = x + dir.dx * i;
+                int ny = y + dir.dy * i;
+                grid[nx][ny] = word[i];
+                solution[nx][ny] = word[i];
+            }
+            placed = 1;
+        }
+    }
+    if (!placed) {
+        printf("Could not place word: %s\n", word);
+    }
 }
